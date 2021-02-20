7710d3a9e5e181f034f840006bbb9eb45e8cf527
Michael Clement <mclement@tushka.k12.ok.us>
2021-02-19 20:32:59 -0600

    Start object chapter

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,20 @@
 // Reverses a string
 function reverse(string) {
-    return string.split("").reverse().join("");
+    return Array.from(string).reverse().join("");
 }
 
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-    let processedContent = string.toLowerCase();
-    return processedContent === reverse(processedContent);
+// Defines a Phrase object
+function Phrase(content) {
+    this.content = content;
+
+    // Returns true for a palindrome, false otherwise.
+    this.palindrome = function palindrome() {
+        let processedContent = this.content.toLowerCase();
+        return processedContent === reverse(processedContent);
+    }
+
+    // Returns uppercase version of phrase
+    this.louder = function louder() {
+        return this.content.toUpperCase();
+    }
 }
